61744257515ec3eecc9fe28d357fd3c42ae16ca6
FVazquezRisso <facundovazquez321@gmail.com>
2023-06-29 15:04:05 -0300

    Conectando a la base de datos

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,27 +1,7 @@
-// const http = require("http");
-// const { getCharById } = require("./controllers/getCharById");
-
-// const PORT = 3001;
-
-// http
-//   .createServer((req, res) => {
-//     console.log(`Server raised in port ${PORT}`);
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-
-//     if (req.url.includes("/rickandmorty/character")) {
-//       const id = req.url.split("/").pop();
-
-//       getCharById(res, +id);
-//       return;
-//     }
-
-//     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-//     res.end("Ruta no válida");
-//   })
-//   .listen(PORT, "localhost");
-
 const express = require("express");
-const { router } = require('./routes/index')
+const { router } = require("./routes/index");
+const { conn } = require("./DB_connection");
+
 const server = express();
 const PORT = 3001;
 
@@ -32,18 +12,21 @@ server.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE"
-  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-server.use(express.json())
+server.use(express.json());
 
-server.use('/rickandmorty', router)
+server.use("/rickandmorty", router);
 
-server.listen(PORT, () => {
-  console.log(`Server raised in port: ${PORT}`);
-
-});
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server raised in port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
